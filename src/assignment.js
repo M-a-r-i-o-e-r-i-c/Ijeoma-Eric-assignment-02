a8be2395d321741036e97ae1fbaf76e85b54302a
Eric Ijeoma <ericijeoma7767@gmail.com>
2022-07-05 15:30:07 +0000

    I attempted Challenges -1 and -2. For Challenge -1, I created a function to sum the numbers of an array and for Challenge -2 I created a function to sum the number of even numbers in an array.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+  let sum = 0;
+    for(let element of arrayOfNumbers){
+        sum += element;
+    }
+    return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +31,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbers){
+  let count = 0;
+    for(let index of arrayOfNumbers){
+        if (index % 2 == 0){
+            count++
+        }
+
+    }
+    return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
